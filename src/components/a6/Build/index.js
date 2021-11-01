327a39c9c235c8dd268fe092e541c6816f90f601
wanglu1207 <67501735+wanglu1207@users.noreply.github.com>
2021-10-31 21:24:14 -0700

    React

--- src/components/a6/Build/index.js
+++ src/components/a6/Build/index.js
@@ -1,21 +1,21 @@
 import React from "react";
 import ExploreScreen from "./ExploreScreen/ExploreScreen";
+import {Link} from "react-router-dom";
 
 const Build = () => {
     return(
         <>
             <ExploreScreen/>
+            <h1>Build!</h1>
+            <Link to="/a6/hello">
+                Hello
+            </Link> |
+            <Link to="/a6/practice">
+                Practice
+            </Link>
         </>
+
     )
 };
 
 export default Build;
-// <>
-//     <h1>Build!</h1>
-//     <Link to="/a6/hello">
-//         Hello
-//     </Link> |
-//     <Link to="/a6/practice">
-//         Practice
-//     </Link>
-// </>
